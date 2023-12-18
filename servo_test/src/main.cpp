398ae760cbe90602149580acecfccec994b4dac1
TrixterBoi <33219323+TrixterBoi@users.noreply.github.com>
2023-12-18 09:31:40 +0100

    FInal Version

--- servo_test/src/main.cpp
+++ servo_test/src/main.cpp
@@ -102,6 +102,40 @@ void setup(){
 }
 
 void loop(){
+
+    if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    int commaIndex = data.indexOf(',');
+
+    // if (commaIndex == -1 || commaIndex == 0 || commaIndex == data.length() - 1) {
+    //   Serial.println("Error: Invalid data format");
+    //   return;
+    // }
+
+    String value1Str = data.substring(0, commaIndex);
+    String value2Str = data.substring(commaIndex + 1);
+
+    int speed = value1Str.toInt();
+    int R = value2Str.toInt();
+    if(R<0){
+        turning = 1;
+        R = abs(R);
+    }
+
+    // if (value1Str.toInt() == 0 && value1Str != "0" || value2Str.toInt() == 0 && value2Str != "0") {
+    //   Serial.println("Error: Non-integer data received");
+    //   return;
+    // }
+
+    // Serial.print("Received Value 1: ");
+    // Serial.print(value1);
+    // Serial.print(", Value 2: ");
+    // Serial.println(value2);
+
+    // // Send back an acknowledgement
+    // Serial.println("ACK: Data Received");
+    }
+
 	// Motors are grouped by function, not leg
 	float time_elapsed = (TCNT2 + timer_overflow*1024)*0.000064; // Load time elapsed since last calculation (accounting for overflow by addig the max value of Timer0 timer_overflow times)
 	timer_overflow = 0;
