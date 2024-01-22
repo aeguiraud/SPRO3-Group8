8549c870be072ca565838f730046f97ecd158e36
TrixterBoi <33219323+TrixterBoi@users.noreply.github.com>
2024-01-22 10:20:50 +0100

    Added code with MKR integration

--- servo_test/src/main.cpp
+++ servo_test/src/main.cpp
@@ -70,8 +70,6 @@ void propel(uint8_t);
 void setup(){
 	Serial.begin(9600);
 	// Serial.println("8 channel Servo test!");
-	Serial.begin(9600);
-	// Serial.println("8 channel Servo test!");
 
 	pwm.begin();
 
@@ -100,6 +98,41 @@ void setup(){
 }
 
 void loop(){
+    
+    if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    int commaIndex = data.indexOf(',');
+
+    if (commaIndex == -1 || commaIndex == 0 || commaIndex == data.length() - 1) {
+      Serial.println("Error: Invalid data format");
+      raving = 1;
+	  return;
+    }
+
+    String value1Str = data.substring(0, commaIndex);
+    String value2Str = data.substring(commaIndex + 1);
+
+    speed = value1Str.toInt();
+    R = value2Str.toInt();
+    if(R<0){
+        turning = 1;
+        R = abs(R);
+    }
+    
+
+    if (value1Str.toInt() == 0 && value1Str != "0" || value2Str.toInt() == 0 && value2Str != "0") {
+      Serial.println("Error: Non-integer data received");
+      return;
+    }
+
+    // Serial.print("Received Value 1: ");
+    // Serial.print(value1);
+    // Serial.print(", Value 2: ");
+    // Serial.println(value2);
+
+    // // Send back an acknowledgement
+    // Serial.println("ACK: Data Received");
+    }
 	// Motors are grouped by function, not leg
 	float time_elapsed = (TCNT2 + timer_overflow*256)*0.000064; // Load time elapsed since last calculation (accounting for overflow by addig the max value of Timer0 timer_overflow times)
 	timer_overflow = 0;
